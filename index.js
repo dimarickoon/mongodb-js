67260f85a9a5e1cc2193755abfaf088dfd6f9acb
dimarickoon <appledimarickoon@gmail.com>
2019-07-25 12:10:28 +0300

    making mongodb connection and reading

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const Https = require('https');
 var TelegramBot = require('node-telegram-bot-api');
-var MongoDB = require('mongodb');
+var MongoDB = require('mongodb').MongoClient;
+
+const token = "901936923:AAFGeTaEcZtlhNCpRkJHo4zvORJmo-dHpCM";
+const bot = new TelegramBot(token,{polling:true});
+
+const url = 'mongodb://localhost:27017'
+
+MongoDB.connect(url, function (err, db)  {
+	var dbo = db.db('test');
+	
+	dbo.collection('Employers', function(err,collection) {
+
+		dbo.collection('Employers').count(function(err,count) {
+			if (err) throw err;
+			console.log("totals rows: "+ count);
+		});
+
+		dbo.collection('Employers').find({}).toArray(function(err,result){
+			if (err) throw err;
+			console.log(result);
+		});
+
+	});
+})
